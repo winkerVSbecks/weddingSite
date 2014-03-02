f9c7303177eebba4b26765b9cb9106999c283bf9
Varun Vachhar <varunvachhar@gmail.com>
2014-03-01 21:33:57 -0500

    re-ordering the lists and minor changes to the text

--- app/scripts/services/foodAndDrink.js
+++ app/scripts/services/foodAndDrink.js
@@ -9,82 +9,87 @@ angular.module('weddingSiteApp')
 				"desc": "You’ll know you’ve found the right place when the scent of chocolate is so strong you can taste it.  Watch the activity in the chocolate laboratory while you sample the goods."
 			},
 			{ 
+				"title": "Bellwoods Brewery",
+				"desc": "Patios are a Toronto summer tradition: this is the perfect spot to enjoy the sunshine while you sample a rotating selection of specialty beers."
+			},
+			{ 
+				"title": "Torito Tapas Bar",
+				"desc": "Make sure to get a table on the back patio and order a sangría!"
+			},
+			{ 
 				"title": "St Lawrence Market",
 				"desc": "Voted the number one food market in the world by National Geographic.  If you go on Saturday morning, you’ll find local baked goods, cheeses, jams and other treats on the north side farmers’ market.  On the south side (across the street), most of the ready-made foods are on the lower level.  A favourite indulgence is the Italian sandwich – veal, chicken or eggplant.  Also be sure to go to Ace Bakery for a pastry dessert (if they haven’t sold out!)"
+			},
+			{ 
+				"title": "Pomegranate",
+				"desc": "Yummy Persian food!  Our favourites: fesanjaan and adas polo with braised lamb shank.  Best to make a reservation."
+			}],
+  		// TWO
+  		[{ 
+				"title": "Wvrst",
+				"desc": "Toronto’s answer to a German brauhaus: this is the place to go for an indulgent meal of gourmet sausage with traditional toppings, duck fat fries and a pint. Best to go on a weeknight (when there’s no line)."
 			},
 			{ 
 				"title": "White Squirrel Coffee Shop",
 				"desc": "A coffee shop named after a local celebrity: the white squirrel that hangs out in Trinity Bellwoods Park.  Most people go for the coffee.  We go for the giant ice cream sandwiches."
 			},
 			{ 
-				"title": "Aft",
-				"desc": "Barbeque!"
-			},
-			{ 
-				"title": "Momofuku Noodle Bar",
-				"desc": "Very rich and yummy noodles"
-			},
-			{ 
-				"title": "Dlish",
-				"desc": "Yes, we have two favourite cupcake places.  Don’t judge us."
-			},
-			{ 
-				"title": "Kensington Market",
-				"desc": "Where the millennial hippies go…  It’s also home to many nice vegetarian restaurants."
-			}],
-  		// TWO
-  		[{ 
-				"title": "Bellwoods Brewery",
-				"desc": "Patios are a Toronto summer tradition: this is the perfect spot to enjoy the sunshine while you sample a rotating selection of specialty beers."
-			},
-			{ 
-				"title": "Pomegranate",
-				"desc": "Yummy Persian food!  Our favourites: fesanjaan and adas polo with braised lamb shank.  Best to make a reservation."
-			},
-			{ 
 				"title": "Golden Turtle",
 				"desc": "Delicious pho and bun"
+			},
+			{ 
+				"title": "Hawthorne",
+				"desc": "Canadian cuisine with local ingredients"
+			},
+			{ 
+				"title": "Aft",
+				"desc": "Barbeque!"
 			},
 			{ 
 				"title": "Asian Legend",
 				"desc": "Dim sum!"
 			},
 			{ 
-				"title": "Nadege",
-				"desc": "Macarons!"
+				"title": "North of Brooklyn",
+				"desc": "Simple, tasty, and one of the cheapest places to have a dinner out.  Avrïl’s favourite is the white pizza.  Varun likes the margherita."
 			},
 			{ 
-				"title": "Brick Street Bakery",
-				"desc": "This is a good spot to have a delicious sandwich while you explore the Distillery District.  Our fave: the pulled pork."
-			},
-			{ 
-				"title": "Pho Hung",
-				"desc": "This is the historic site of our first dinner together!  It’s also a good spot for an inexpensive meal after your stroll through Chinatown."
+				"title": "Momofuku Noodle Bar",
+				"desc": "Very rich and yummy noodles"
 			}],
   		// THREE
   		[{ 
-				"title": "Torito Tapas Bar",
-				"desc": "Make sure to get a table on the back patio and order a sangría!"
-			},
-			{ 
-				"title": "Wvrst",
-				"desc": "Toronto’s answer to a German brauhaus: this is the place to go for an indulgent meal of gourmet sausage with traditional toppings, duck fat fries and a pint. Best to go on a weeknight (when there’s no line)."
-			},
-			{ 
-				"title": "Hawthorne",
-				"desc": "Canadian cuisine"
-			},
-			{ 
-				"title": "North of Brooklyn",
-				"desc": "Simple, tasty, and one of the cheapest places to have a dinner out.  Avrïl’s favourite is the white pizza.  Varun likes the margherita."
+				"title": "Nadege",
+				"desc": "Macarons!"
 			},
 			{ 
 				"title": "Sweet Bliss Baking Company",
 				"desc": "Cupcakes!  Need we say more?"
 			},
 			{ 
+				"title": "Dlish",
+				"desc": "Yes, we have two favourite cupcake places.  Don’t judge us."
+			},
+			{ 
+				"title": "Brick Street Bakery",
+				"desc": "This is a good spot to have a delicious sandwich while you explore the Distillery District.  Our fave: the pulled pork."
+			},
+			{ 
 				"title": "Black Canary Espresso",
 				"desc": "Italian-style coffee"
+			},
+			{ 
+				"title": "Kensington Market",
+				"desc": "Where the millennial hippies go…  It’s also home to many nice vegetarian restaurants."
+			},
+			{ 
+				"title": "Pho Hung",
+				"desc": "This is the historic site of our first dinner together!  It’s also a good spot for an inexpensive meal after your stroll through Chinatown."
 			}]
 		
   	]);
+			
+			
+			
+			
+			
--- app/scripts/services/thingsToDo.js
+++ app/scripts/services/thingsToDo.js
@@ -8,79 +8,61 @@ angular.module('weddingSiteApp')
 				"desc": "This is where we first met and has since become one of our favourite spots in the city."
 			},
 			{
-				"title": "Royal Ontario Museum",
-				"desc": "Dinosaurs!"
-			},
-			{ 
-				"title": "Toronto Island",
-				"desc": "It may seem out of the way, but the island is one of the best spots to enjoy the Toronto summer!  It’s just a short ride from the Jack Layton Ferry Terminal at the south end of Bay Street to Centre Island.  Once there, the best way to get away from the crowds (likely if it’s a weekend) is to rent a bicycle – traditional, tandem, or four-seater – and explore.  There are beaches (including one nude beach for the adventurous), nice shady trees for picnics, and a gorgeous view of the Toronto skyline."
-			},
-			{ 
-				"title": "Canadian Opera Company",
-				"desc": "Incentive for our younger guests: if you’re under thirty, you can get tickets for $22."
-			},
-			{ 
-				"title": "Trinity Bellwoods Park",
-				"desc": "Where the hipsters go to beat the heat and drink out of brown bags."
-			}],
-  		// TWO
-  		[{
 				"title": "Distillery District",
 				"desc": "This is one of our favourite spots for a date.  You can have some gelato from SOMA while you wander around to look at the shops.  Varun’s favourite is Bergo Designs.  Avrïl’s fave is Distill."
+			},
+			{
+				"title": "Beaches",
+				"desc": "Yes, there is a beach in Toronto!  If you’re having trouble finding it, take the 501 streetcar east and follow the people wearing sun hats and bathing suits.  Bring a towel, relax, and see who among you is brave enough to go into the water."
+			},
+			{
+				"title": "Royal Ontario Museum",
+				"desc": "Dinosaurs!"
 			},
 			{
 				"title": "Magic Pony",
 				"desc": "Cute art and design store, with a gallery in the back."
 			},
 			{ 
-				"title": "Lightbox",
-			"desc": "Artsy films!"
-			},
-			{ 
-				"title": "Chinatown",
-			"desc": "This is one of Varun’s favourite spots because the main drag on Spadina reminds him a bit of where he grew up in Delhi.  He also loves the stores with “geeky stuff”."
-			},
-			{ 
-				"title": "Dragon City Mall",
-			"desc": "Varun made me put this in because he loves the name."
-			},
-			{
-				"title": "Beaches",
-				"desc": "Yes, there is a beach in Toronto!  If you’re having trouble finding it, take the 501 streetcar east and follow the people wearing sun hats and bathing suits.  Bring a towel, relax, and see who among you is brave enough to go into the water."
-			}],
-  		// THREE
-  		[{ 
 				"title": "Active Surplus",
 				"desc": "This is Varun’s personal heaven.  You’ll know you’ve come to the right place when you see the big gorilla outside.  Things we have seen for sale here: a traffic light, doll parts, loose ball bearings, and the perfect fabric to make rainbow suspenders."
+			}],
+  		// TWO
+  		[{ 
+				"title": "Toronto Island",
+				"desc": "It may seem out of the way, but the island is one of the best spots to enjoy the Toronto summer!  It’s just a short ride from the Jack Layton Ferry Terminal at the south end of Bay Street to Centre Island.  Once there, the best way to get away from the crowds (likely if it’s a weekend) is to rent a bicycle – traditional, tandem, or four-seater – and explore.  There are beaches (including one nude beach for the adventurous), nice shady trees for picnics, and a gorgeous view of the Toronto skyline."
+			},
+			{ 
+				"title": "Lightbox",
+				"desc": "Artsy films!"
 			},
 			{ 
 				"title": "Power Plant",
 				"desc": "Contemporary art!"
 			},
 			{ 
+				"title": "Canadian Opera Company",
+				"desc": "Incentive for our younger guests: if you’re under thirty, you can get tickets for $22."
+			},
+			{ 
+				"title": "Chinatown",
+				"desc": "This is one of Varun’s favourite spots because the main drag on Spadina reminds him a bit of where he grew up in Delhi.  He also loves the stores with \"geeky stuff\"."
+			},
+			{ 
+				"title": "Dragon City Mall",
+			"desc": "Varun made me put this in because he loves the name."
+			}],
+  		// THREE
+  		[{ 
 				"title": "Yorkville",
 				"desc": "For our fancier guests: you can go shopping at places like Tiffany, Cartier, Prada and Chanel. For the rest, do like Varun and I: have a seat on the big rock on Cumberland St and watch the fancy people go by."
+			},
+			{ 
+				"title": "Trinity Bellwoods Park",
+				"desc": "Where the hipsters go to beat the heat and drink out of brown bags."
 			},
 			{ 
 				"title": "Measurement Park",
 				"desc": "If you’re headed to the beaches, take a stroll through Measurement Park along the way.   Hint: the name is apt."
 			}]
 		]);
-
-
-
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
