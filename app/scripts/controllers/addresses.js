445491cb2de62fb8cb2afa6ca60a4ce317e93561
Varun Vachhar <varunvachhar@gmail.com>
2014-03-29 00:12:24 -0400

    added RSVP

--- app/scripts/controllers/addresses.js
+++ app/scripts/controllers/addresses.js
@@ -1,47 +1,45 @@
 'use strict';
 
 angular.module('weddingSiteApp')
-  .controller('AddressesCtrl', ['$scope', '$firebase', '$location', '$firebaseSimpleLogin', '$rootScope' , function ($scope, $firebase, $location, $firebaseSimpleLogin, $rootScope) {
+  .controller('AddressesCtrl', ['$scope', '$firebase', '$location', '$firebaseSimpleLogin', '$rootScope',
+    function($scope, $firebase, $location, $firebaseSimpleLogin, $rootScope) {
 
-  	// LINK TO FIREBASE
-  	var ref = new Firebase('https://vachharjacobson.firebaseio.com');
-  	$scope.isLoggedIn = false;
-
-
-    // LOGIN OBJECT
-  	$scope.loginObj = $firebaseSimpleLogin(ref);
+      // LINK TO FIREBASE
+      var ref = new Firebase('https://vachharjacobson.firebaseio.com/addresses');
+      $scope.isLoggedIn = false;
 
 
-  	// Check for Login
-  	$scope.$watch('loginObj.user', function () {
-
-  		if ( $scope.isAdmin($scope.loginObj.user) ) {
-  			$scope.addresses = $firebase(ref);
-  			$scope.isLoggedIn = true;
-  		}
-  	
-  	});
+      // LOGIN OBJECT
+      $scope.loginObj = $firebaseSimpleLogin(ref);
 
 
-  	// Is the user admin
-  	$scope.isAdmin = function (user) {
+      // Check for Login
+      $scope.$watch('loginObj.user', function() {
 
-  		return user 
-  					&& user.id === '2' 
-  					&& user.email === 'varunvachhar@gmail.com'
-  					&& user.provider === 'password'
-  					&& user.uid === 'simplelogin:2';
-  	};
+        if ($scope.isAdmin($scope.loginObj.user)) {
+          $scope.addresses = $firebase(ref);
+          $scope.isLoggedIn = true;
+        }
+
+      });
 
 
-  	// LOGOUT
-  	$scope.logout = function () {
-			$scope.loginObj.$logout();
-		};
+      // Is the user admin
+      $scope.isAdmin = function(user) {
 
-		// Redirect
-		$rootScope.$on("$firebaseSimpleLogin:logout", function(e, user) {
-		  $location.path('/login');
-		});
+        return user && user.id === '2' && user.email === 'varunvachhar@gmail.com' && user.provider === 'password' && user.uid === 'simplelogin:2';
+      };
 
-  }]);
+
+      // LOGOUT
+      $scope.logout = function() {
+        $scope.loginObj.$logout();
+      };
+
+      // Redirect
+      $rootScope.$on("$firebaseSimpleLogin:logout", function(e, user) {
+        $location.path('/login');
+      });
+
+    }
+  ]);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,65 +1,119 @@
 'use strict';
 
 angular.module('weddingSiteApp')
-  .controller('MainCtrl', ['$scope', '$firebase', '$timeout', '$http', '$location', '$anchorScroll', 'thingsToDo', 'foodAndDrink', function ($scope, $firebase, $timeout, $http, $location, $anchorScroll, thingsToDo, foodAndDrink) {
+  .controller('MainCtrl', ['$scope', '$firebase', '$timeout', '$http', '$location', '$anchorScroll', 'thingsToDo', 'foodAndDrink',
+    function($scope, $firebase, $timeout, $http, $location, $anchorScroll, thingsToDo, foodAndDrink) {
 
-    $scope.thingsToDo = thingsToDo;
-    $scope.foodAndDrink = foodAndDrink;
+      $scope.thingsToDo = thingsToDo;
+      $scope.foodAndDrink = foodAndDrink;
 
-    // COLLAPSE
-    $scope.showAccommodation = false;
+      // COLLAPSE
+      $scope.showAccommodation = false;
 
-    // SCROLL TO SECTIONS
-    $scope.scrollTo = function(id) {
-      $location.hash(id);
-      $anchorScroll();
-    };
+      // SCROLL TO SECTIONS
+      $scope.scrollTo = function(id) {
+        $location.hash(id);
+        $anchorScroll();
+      };
 
-  	// LINK TO FIREBASE
-  	var ref = new Firebase('https://vachharjacobson.firebaseio.com');
-  	
-  	// ADDRESSES
-  	$scope.addresses = $firebase(ref);
-  	$scope.addressForm = { error : false, success : false };
+      // -----------------------------
+      // RSVP
+      // -----------------------------
+      var rsvpRef = new Firebase('https://vachharjacobson.firebaseio.com/rsvp');
+      $scope.rsvp = $firebase(rsvpRef);
+      $scope.rsvpForm = {
+        error: false,
+        success: false
+      };
+      // Submit the rsvp
+      $scope.submitRsvp = function() {
 
-  	// Submit the address
-  	$scope.submitAddress = function () {
+        $scope.rsvpForm.success = false;
 
-  		$scope.addressForm.success = false;
+        if (!$scope.rsvpForm.name || $scope.rsvpForm.name === '') {
 
-  		if(!$scope.addressForm.name || $scope.addressForm.name === '') {
-  			
-  			$scope.addressForm.errMessage = 'Name is required.';
-  			$scope.addressForm.error = true;
-  		
-  		} else if(!$scope.addressForm.address || $scope.addressForm.address === '') {
+          $scope.rsvpForm.errMessage = 'Name is required.';
+          $scope.rsvpForm.error = true;
 
-  			$scope.addressForm.errMessage = 'Address is required.';
-  			$scope.addressForm.error = true;
+        } else if (!$scope.rsvpForm.guestCount || $scope.rsvpForm.guestCount === '') {
 
-  		} else {
+          $scope.rsvpForm.errMessage = 'Guest count is required.';
+          $scope.rsvpForm.error = true;
 
-  			$scope.addressForm.error = false;
-  			$scope.addressForm.errMessage = '';
-  			$scope.addressForm.success = true;
+        } else {
+          $scope.rsvpForm.error = false;
+          $scope.rsvpForm.errMessage = '';
+          $scope.rsvpForm.success = true;
 
-  			// Save to firebase
-  			$scope.addresses.$add({
-  				name: $scope.addressForm.name, 
-  				address: $scope.addressForm.address
-  			}, function (data) {
-          // Reset Form
-          $scope.addressForm.name = '';
-          $scope.addressForm.address = '';
-        });
+          // Save to firebase
+          $scope.rsvp.$add({
+            name: $scope.rsvpForm.name,
+            guestCount: $scope.rsvpForm.guestCount,
+            note: $scope.rsvpForm.note
+          }).then(function(data) {
+            // Reset Form
+            $scope.rsvpForm.name = '';
+            $scope.rsvpForm.note = '';
+            $scope.rsvpForm.guestCount = '';
+          });
 
-  			$timeout(function () {
-  				$scope.addressForm.success = false;
-          $scope.addressForm.error = '';	
-  			}, 3000);
+          $timeout(function() {
+            $scope.rsvpForm.success = false;
+            $scope.rsvpForm.error = '';
+          }, 3000);
 
-  		}
+        }
 
-  	}; 
+      };
 
-  }]);
+      // -----------------------------
+      // ADDRESSES
+      // -----------------------------
+      var addressesRef = new Firebase('https://vachharjacobson.firebaseio.com/addresses');
+      $scope.addresses = $firebase(addressesRef);
+      $scope.addressForm = {
+        error: false,
+        success: false
+      };
+      // Submit the address
+      $scope.submitAddress = function() {
+
+        $scope.addressForm.success = false;
+
+        if (!$scope.addressForm.name || $scope.addressForm.name === '') {
+
+          $scope.addressForm.errMessage = 'Name is required.';
+          $scope.addressForm.error = true;
+
+        } else if (!$scope.addressForm.address || $scope.addressForm.address === '') {
+
+          $scope.addressForm.errMessage = 'Address is required.';
+          $scope.addressForm.error = true;
+
+        } else {
+
+          $scope.addressForm.error = false;
+          $scope.addressForm.errMessage = '';
+          $scope.addressForm.success = true;
+
+          // Save to firebase
+          $scope.addresses.$add({
+            name: $scope.addressForm.name,
+            address: $scope.addressForm.address
+          }).then(function(data) {
+            // Reset Form
+            $scope.addressForm.name = '';
+            $scope.addressForm.address = '';
+          });
+
+          $timeout(function() {
+            $scope.addressForm.success = false;
+            $scope.addressForm.error = '';
+          }, 3000);
+
+        }
+
+      };
+
+    }
+  ]);
